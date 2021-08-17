d5ea9b318eed271206d9b2354ecda99cbcd08c90
ryougiitachi-hm <ryougiitachi-hm@hotmail.com>
2021-08-17 17:53:38 +0800

    [lucenc-sample] Improved writing indices.

--- lucene-sample/src/main/java/per/itachi/java/lucene/sample/SampleApplication.java
+++ lucene-sample/src/main/java/per/itachi/java/lucene/sample/SampleApplication.java
@@ -1,12 +1,30 @@
 package per.itachi.java.lucene.sample;
 
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.List;
 
 public class SampleApplication {
+
+    private final Logger logger = LoggerFactory.getLogger(SampleApplication.class);
 
     public static void main(String[] args) {
 
@@ -33,7 +51,28 @@ public class SampleApplication {
             // logger.error
             return;
         }
-        for (String inputDocumentPath : inputDocumentPaths) {
+        // initialise config.
+        Analyzer analyzer = new StandardAnalyzer();
+        IndexWriterConfig config = new IndexWriterConfig(analyzer);
+        config.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
+
+        // start writing indices
+        try(Directory dirOutputIndices = FSDirectory.open(Paths.get(outputIndicesPath));
+            IndexWriter writer = new IndexWriter(dirOutputIndices, config)) {
+            logger.info("Start writing index into directory {}. ", dirOutputIndices);
+            for (String inputDocumentPath : inputDocumentPaths) {
+                try(InputStream bis = new BufferedInputStream(Files.newInputStream(Paths.get(inputDocumentPath)))) {
+                    Document document = new Document();
+                    document.add(new StringField("file_name",
+                            Paths.get(inputDocumentPath).getFileName().toString(), Field.Store.YES));
+                    document.add(new TextField("content",
+                            new BufferedReader(new InputStreamReader(bis, StandardCharsets.UTF_8))));
+                    writer.addDocument(document);
+                }
+            }
+        }
+        catch (IOException e) {
+            logger.error("", e);
         }
     }
 }
---
+++ lucene-sample/src/main/java/per/itachi/java/lucene/sample/entity/PostDocument.java
@@ -0,0 +1,6 @@
+package per.itachi.java.lucene.sample.entity;
+
+public class PostDocument {
+
+    public static final String FLD_FILE_NAME = "file_name";
+}
