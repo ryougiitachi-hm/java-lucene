6ba6e797268fee34655f0ed7df043d22851d859c
ryougiitachi-hm <ryougiitachi@hotmail.com>
2021-08-29 12:12:25 +0800

    [lucene-sample] Tested URL.

---
+++ lucene-sample/src/test/java/per/itachi/java/jdk/net/URLTest.java
@@ -0,0 +1,19 @@
+package per.itachi.java.jdk.net;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+@RunWith(MockitoJUnitRunner.class)
+public class URLTest {
+
+    @Test
+    public void testURL() throws MalformedURLException {
+        URL url = new URL("ftp://jkkjk.jjl.com/aj/b?ui=1&ui=1&ai=1&");
+        System.out.printf("url=%s, protocol=%s, host=%s, port=%s, path=%s, query=%s, defaultPort=%d %n",
+                url, url.getProtocol(), url.getHost(), url.getPort(), url.getPath(), url.getQuery(), url.getDefaultPort());
+    }
+}
